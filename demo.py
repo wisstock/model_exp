861a8f61a090d6e5e023243d393db83137dd5f53
wisstock <omnia.fatum@gmail.com>
2022-03-08 16:06:21 +0200

    1st commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Copyright © 2022 Borys Olifirov
+Experiments with calcium sensors kinetic models.
+
+Conda env 'model'
+
+"""
+
+import numpy as np
+import sympy as sym
+import matplotlib.pyplot as plt
+
